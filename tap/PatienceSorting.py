788616802ae4b26867f51757978e7cbcee8b8acf
mihainsto <44275480+mihainsto@users.noreply.github.com>
2019-11-04 21:43:34 +0200

    Update PatienceSorting.py

--- tap/PatienceSorting.py
+++ tap/PatienceSorting.py
@@ -6,24 +6,36 @@
 # 13 12
 # 15
 
+
+
 import heapq
+def binary_search(a, x, lo=0, hi=None):
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if a[mid][-1] < x:
+            lo = mid+1
+        else:
+            hi = mid
+    return lo
+
+
 
 v = [11,13,10,15,12,7]
 sub = []
-for _ in range(len(v)):
-    sub.append([])
-for el in v:
-    for i in range(0,len(v)):
-        if(len( sub[i] )==0):
-            sub[i].append(el)
-            break
-        else:
-            if(sub[i][-1] > el):
-                sub[i].append(el)
-                break
 
-sub2 = [x for x in sub if x != []]
-sub = sub2
+for el in v:
+    i = binary_search(sub, el)
+    if i  >=  len(sub):
+        sub.append([])
+        sub[i].append(el)
+    else:
+        if sub[i][-1] > el:
+            sub[i].append(el)
+
+
+
 print(sub)
 for i in range(len(sub)):
     sub[i].reverse()
