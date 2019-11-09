bafb28ef7207f61f5a68b97ac1426845e5b41361
mihainsto <44275480+mihainsto@users.noreply.github.com>
2019-11-09 15:42:27 +0200

    Update closestPoints.py

--- tap/closestPoints.py
+++ tap/closestPoints.py
@@ -19,7 +19,9 @@ def plotWithLineOn(x):
 def distance(a, b):
     return math.sqrt((a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]))
 
-def bruteForce():
+def bruteForce(points):
+    if len(points) < 2:
+        return -1
     minDist = distance(points[0],points[1])
     minPoints = (points[0],points[1])
     for point1 in points:
@@ -28,18 +30,30 @@ def bruteForce():
             if minDist > distance(point1,point2) and point1 != point2:
                 minDist = distance(point1,point2)
                 minPoints = (point1,point2)
-    print(minDist)
-    print(minPoints)
+    return minDist
+    #print(minPoints)
 
+def stripClose(points, line, mindist):
+    minstrip = mindist
+    for i in range(len(points)):
+        if points[i][0] > line-mindist:
+            #print(points[i])
+            for j in range(i, len(points)):
+                #print(points[j])
+                if points[j][0] > line and points[j][1] > points[i][1] and points[j][0] < line+mindist:
+                    minstrip = min(minstrip, distance(points[i], points[j]))
+        if points[i][0] > line+mindist:
+            break
+    return minstrip
 
 def divimp(v):
-    if len(v) == 2:
-        return distance(v[0],v[1])
+    if len(v) <= 3:
+        return bruteForce(v)
     left = v[0][0]
     right = v[len(v) - 1][0]
 
     middle = (left+right)/2
-    plotWithLineOn(middle)
+    #plotWithLineOn(middle)
     ind = 0
     for ind in range(len(v)):
         if v[ind][0] >= middle:
@@ -47,13 +61,23 @@ def divimp(v):
 
     lowRight = divimp(v[ind:])
     lowLeft = divimp(v[:ind])
-    if lowLeft < lowRight:
-        return lowLeft
-    return lowRight
+    lowDist = min(lowRight, lowLeft)
+    if lowRight == -1:
+        lowDist = lowLeft
+    elif lowLeft == -1:
+        lowDist = lowRight
+    stripDist = stripClose(points, middle, lowDist)
+    lowDist = min(lowDist, stripDist)
+    return lowDist
 
+points.append((46, 3))
+points.append((52, 10))
 
 points.sort()
 print(points)
 
-#plotWithLineOn(-1)
-divimp(points)
+d = 5
+
+#plotWithLineOn(50)
+
+print(divimp(points))
